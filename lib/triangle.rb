149f3a7aa333f09513eabd2846e27f050c64b21d
hmalik88 <hbmalik88@gmail.com>
2018-10-16 16:52:41 -0400

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,46 @@
+require 'pry'
 class Triangle
-  # write code here
+  
+  def initialize(x, y, z)
+  	@x = x
+  	@y = y
+  	@z = z
+  end
+
+	def validate_triangle
+		real_or_not = []
+		if @x == 0 && @y == 0 && @z == 0
+			return true
+		end
+		if @x + @y <= @z || @x + @z <= @y || @y + @z <= @x
+			return true
+		end
+		[@x, @y, @z].each do |side|
+				if side > 0
+				real_or_not << true
+				else 
+				real_or_not << false
+				end 
+		end
+		real_or_not.include?(false)
+ 	end
+
+ 	def kind
+ 		if validate_triangle == true
+ 			raise TriangleError
+ 		else
+ 			if @x == @y && @x == @z
+ 				return :equilateral
+ 			elsif @x == @y || @x == @z || @y == @z
+ 				return :isosceles
+ 			else
+ 				return :scalene
+ 			end
+ 		end
+ 	end
+ 	
+ 	class TriangleError < StandardError
+ 	end
+
 end
+
